72de3f10febeb0859042a231f45438b0440e9bfd
Kodev <jonadotnet@gmail.com>
2016-10-20 00:07:08 -0500

    Update statics route

--- Transportes/settings.py
+++ Transportes/settings.py
@@ -133,6 +133,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = 'static'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 DATE_INPUT_FORMATS = ('%d-%m-%Y', '%d/%m/%Y', '%Y-%m-%d')
 
 LOGIN_REDIRECT_URL = '/'
